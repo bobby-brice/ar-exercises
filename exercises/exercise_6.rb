b92bdc0c1a8083f40ab09438cf7f3bcb0091810f
bobby-brice <bobby.brice@gmail.com>
2020-07-20 22:47:23 +0000

    add validation

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Bobby", last_name: "Brice", hourly_rate: 70)
+@store1.employees.create(first_name: "Jim", last_name: "Tytler", hourly_rate: 101)
+@store1.employees.create(first_name: "Colin", last_name: "Mummery", hourly_rate: 200)
+@store1.employees.create(first_name: "Andrew", last_name: "Wishart", hourly_rate: 60)
+@store2.employees.create(first_name: "Steve", last_name: "French", hourly_rate: 100)
+@store2.employees.create(first_name: "Rob", last_name: "Herron", hourly_rate: 200)
